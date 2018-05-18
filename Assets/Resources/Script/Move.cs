5c7de6e48968d702bcdbf75f615f009cdc7ca654
David Otero <doterog@gmail.com>
2018-05-18 18:08:52 +0200

    Script mov i timer

---
+++ Assets/Resources/Script/Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float tiempo = 2.0f;
+    public float vel = 5;
+    public float posInicial = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Update()
+    {
+
+        tiempo = tiempo - 1 * Time.deltaTime;
+
+        if (tiempo >= 2.0f)
+        {
+            movimiento();
+        }
+
+        if (tiempo <= 0)
+        {
+            tiempo = 3.0f;
+        }
+
+    }
+
+    public void movimiento()
+    {
+
+        if (GameObject.FindGameObjectWithTag("Enemigo"))
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+            GameObject.FindGameObjectWithTag("Enemigo").transform.position, 0.1f);
+
+        }
+        
+    }
+}
---
+++ Assets/Resources/Script/timer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour {
+
+    public GUIText contador;
+    public float tiempo = 0.0f;
+
+    public void Update()
+    {
+        tiempo = tiempo - 1*Time.deltaTime;
+        contador.text = "" + tiempo;
+    }
+}
