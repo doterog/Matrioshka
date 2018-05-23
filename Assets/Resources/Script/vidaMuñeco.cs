71ca662981c30f133edb85a4bb28074f52266448
David Otero <doterog@gmail.com>
2018-05-23 17:17:47 +0200

    spawn

---
+++ Assets/Resources/Script/vidaMuñeco.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vidaMuñeco : MonoBehaviour {
+    public int vida = 1;
+    void Update()
+    {
+        if (this.transform.position.y <= -100)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (this.tag.Equals("BLUE_Babuska"))
+        {
+            if (GameObject.FindGameObjectWithTag("RED_Babuska"))
+            {
+                vida--;
+                if (vida == 0)
+                {
+                    Destroy(this.gameObject);
+                    Destroy(collision.gameObject);
+                }
+            }
+        }
+        else
+        {
+            if (GameObject.FindGameObjectWithTag("BLUE_Babuska"))
+            {
+                vida--;
+                if (vida == 0)
+                {
+                    Destroy(this.gameObject);
+                    Destroy(collision.gameObject);
+                }
+            }
+        }
+    }
+}
