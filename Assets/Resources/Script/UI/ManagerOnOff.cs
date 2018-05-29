e8f754ab05ffea1e9eff95bd86365e7c0afc24e6
Malmortius <viktor.gom@gmail.com>
2018-05-29 20:54:19 +0200

    UI_Buttons

---
+++ Assets/Resources/Script/UI/ManagerOnOff.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerOnOff : MonoBehaviour
+{
+    public Button but;
+    public Sprite OffSprite;
+    public Sprite OnSprite;
+
+
+    public void OnOff()
+    {
+        if (but.image.sprite == OnSprite)
+            but.image.sprite = OffSprite;
+        else
+        {
+            but.image.sprite = OnSprite;
+        }
+    }
+
+    public void Off()
+    {
+            but.image.sprite = OffSprite;
+    }
+
+}
---
+++ Assets/Resources/Script/UI/VarButtons.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnOff_Image : MonoBehaviour {
+
+    public Sprite Babu_OffSprite;
+    public Sprite Babu_OnSprite;
+    public Button Babu_Button;
+
+}
